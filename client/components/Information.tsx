e088a6f5bdb7ab3378561a5b978bf2756d36d56e
Suwon Jeong <integer.won@gmail.com>
2024-05-10 14:21:38 +1200

    modify font size and change image for story

--- client/components/Information.tsx
+++ client/components/Information.tsx
@@ -1,20 +1,25 @@
-import { InstagramIcon } from './Icon'
+import { InstagramIcon, MapIcon, PhoneIcon } from './Icon'
 
 const Information = () => {
   return (
     <div id="information">
       <p className="information-title">Contact</p>
       <p className="information-contact">
-        <a href="tel:02040829119" rel="noopener noreferrer">
-          📞 020 40829119
+        <a
+          href="tel:02040829119"
+          rel="noopener noreferrer"
+          className="information-phone"
+        >
+          <PhoneIcon /> 020 40829119
         </a>
         <br />
         <a
           href="https://www.google.com/maps/search/?api=1&query=3/146+Sunnynook+Road"
           target="_blank"
           rel="noopener noreferrer"
+          className="information-address"
         >
-          📍 3/146 Sunnynook Road
+          <MapIcon /> 3/146 Sunnynook Road
         </a>
       </p>
       <p className="information-title">Hours</p>
@@ -22,11 +27,17 @@ const Information = () => {
         OPEN
         <br />
         Tuesday ~ Saturday <br />
-        11:30am - 8:30pm (Break 3pm-5pm) <br /> <br />
+        11:30am - 8:30pm (Break 3pm-5pm)
+        <br />
+        <span>[ Last Order 2:30pm ]</span>
+      </p>
+      <p className="information-hours">
         CLOSED <br />
         Monday, Sunday & Public Holiday
+        <br />
+        <span>[ Last Order 8:00pm ]</span>
       </p>
-      <a href='https://www.instagram.com/manoaheatery/'>
+      <a href="https://www.instagram.com/manoaheatery/">
         <InstagramIcon />
       </a>
     </div>
