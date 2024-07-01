27cf7915fd9ce9cf2cf04fe4f3a3e6da78c8de77
manoah <manoah.web@gmail.com>
2024-07-01 23:43:43 +0900

    change menu file url (new menu 0624)

--- client/components/Menu.tsx
+++ client/components/Menu.tsx
@@ -1,8 +1,8 @@
 import menu from '../images/menu.jpg'
 
 const cvFileUrl =
-  'https://drive.google.com/file/d/1AYzxYkSrIBNTwqyNI4ZC3hTwQM4sFAct/view?usp=drive_link'
-const cvFileName = 'manoah-menu.pdf'
+    'https://drive.google.com/file/d/13SGae8rV7YRptyAV388S1yVE8m2brQgO/view'
+const cvFileName = 'Menu_A4_2406_FINAL.pdf'
 
 const Menu = () => {
   const downloadPDF = (url: string) => {
